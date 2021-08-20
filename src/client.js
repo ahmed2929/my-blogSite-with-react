f3fc80329ce8ad7f1430e3c5d6955d64a4b4d34b
ahmed khaled <34711604+ahmed2929@users.noreply.github.com>
2021-08-20 21:05:05 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-    projectId:"",
-    dataset:"",
+    projectId:"yrhutsqp",
+    dataset:"production",
     useCdn:true
 })
